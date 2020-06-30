2c180db94fe5b6a9695a19faab301b6ee32e5129
Silverdimond <46320280+Silverdimond@users.noreply.github.com>
2020-06-30 22:27:31 +0200

    include up.txt down.txt left.txt and right.txt

--- twitch chat controlls/Form1.cs
+++ twitch chat controlls/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -16,8 +17,10 @@ namespace twitch_chat_controlls
         [DllImport("user32.dll")]
         public static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vlc);
         int a = 0, b = 0, c = 0, d = 0;
-
-
+        List<string> up = new List<string>();
+        List<string> down = new List<string>();
+        List<string> left = new List<string>();
+        List<string> right = new List<string>();
         public Form1()
         {
            // See: https://msdn.microsoft.com/en-us/library/windows/desktop/dd375731(v=vs.85).aspx
@@ -42,6 +45,7 @@ namespace twitch_chat_controlls
                 this.Handle, 4, 0x0000, FirstHotKeyKeyabc
             );
             InitializeComponent();
+            loadconf();
         }
 
 
@@ -51,7 +55,170 @@ namespace twitch_chat_controlls
         {
 
         }
+        void loadconf()
+        {
+            try
+            {
+                using (StreamReader upc = new StreamReader(Environment.CurrentDirectory+@"\config\up.txt"))
+                {
+                    while (!upc.EndOfStream)
+                    {
+                       
+                       up.Add(upc.ReadLine());
+                    }
+                }
+            }
+            catch (Exception e)
+            {
 
+
+                MessageBoxButtons buttons = MessageBoxButtons.RetryCancel;
+                DialogResult result;
+
+                // Displays the MessageBox.
+                result = MessageBox.Show("ERROR:"+e.ToString(), "Error", buttons);
+                if (result == System.Windows.Forms.DialogResult.Retry)
+                {
+                    loadconf();
+                    return;
+                }
+
+            }
+            if (up.Count==0)
+            {
+                MessageBoxButtons buttons = MessageBoxButtons.RetryCancel;
+                DialogResult result;
+
+                // Displays the MessageBox.
+                result = MessageBox.Show(@"ERROR: FILE `config\up.txt` IS EMPTY", "Error", buttons);
+                if (result == System.Windows.Forms.DialogResult.Retry)
+                {
+                    loadconf();
+                    return;
+                }
+            }
+            try
+            {
+                using (StreamReader upc = new StreamReader(Environment.CurrentDirectory + @"\config\down.txt"))
+                {
+                    while (!upc.EndOfStream)
+                    {
+
+                        down.Add(upc.ReadLine());
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+
+
+                MessageBoxButtons buttons = MessageBoxButtons.RetryCancel;
+                DialogResult result;
+
+                // Displays the MessageBox.
+                result = MessageBox.Show("ERROR:" + e.ToString(), "Error", buttons);
+                if (result == System.Windows.Forms.DialogResult.Retry)
+                {
+                    loadconf();
+                    return;
+                }
+
+            }
+            if (down.Count == 0)
+            {
+                MessageBoxButtons buttons = MessageBoxButtons.RetryCancel;
+                DialogResult result;
+
+                // Displays the MessageBox.
+                result = MessageBox.Show(@"ERROR: FILE `config\down.txt` IS EMPTY", "Error", buttons);
+                if (result == System.Windows.Forms.DialogResult.Retry)
+                {
+                    loadconf();
+                    return;
+                }
+            }
+            try
+            {
+                using (StreamReader upc = new StreamReader(Environment.CurrentDirectory + @"\config\left.txt"))
+                {
+                    while (!upc.EndOfStream)
+                    {
+
+                        left.Add(upc.ReadLine());
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+
+
+                MessageBoxButtons buttons = MessageBoxButtons.RetryCancel;
+                DialogResult result;
+
+                // Displays the MessageBox.
+                result = MessageBox.Show("ERROR:" + e.ToString(), "Error", buttons);
+                if (result == System.Windows.Forms.DialogResult.Retry)
+                {
+                    loadconf();
+                    return;
+                }
+
+            }
+            if (left.Count == 0)
+            {
+                MessageBoxButtons buttons = MessageBoxButtons.RetryCancel;
+                DialogResult result;
+
+                // Displays the MessageBox.
+                result = MessageBox.Show(@"ERROR: FILE `config\left.txt` IS EMPTY", "Error", buttons);
+                if (result == System.Windows.Forms.DialogResult.Retry)
+                {
+                    loadconf();
+                    return;
+                }
+            }
+            try
+            {
+                using (StreamReader upc = new StreamReader(Environment.CurrentDirectory + @"\config\right.txt"))
+                {
+                    while (!upc.EndOfStream)
+                    {
+
+                       right.Add(upc.ReadLine());
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+
+
+                MessageBoxButtons buttons = MessageBoxButtons.RetryCancel;
+                DialogResult result;
+
+                // Displays the MessageBox.
+                result = MessageBox.Show("ERROR:" + e.ToString(), "Error", buttons);
+                if (result == System.Windows.Forms.DialogResult.Retry)
+                {
+                    loadconf();
+                    return;
+                }
+
+            }
+            if (right.Count == 0)
+            {
+                MessageBoxButtons buttons = MessageBoxButtons.RetryCancel;
+                DialogResult result;
+
+                // Displays the MessageBox.
+                result = MessageBox.Show(@"ERROR: FILE `config\right.txt` IS EMPTY", "Error", buttons);
+                if (result == System.Windows.Forms.DialogResult.Retry)
+                {
+                    loadconf();
+                    return;
+                }
+            }
+            return;
+        }
         protected override void WndProc(ref Message m)
         {
             if (m.Msg == 0x0312)
@@ -60,56 +227,60 @@ namespace twitch_chat_controlls
                 switch (id)
                 {
                     case 1:
-                        if(a!=1)
+                        if(a!=up.Count-1)
                         {
-                            SendKeys.Send("du");
-                            a = 1;
+                            SendKeys.Send(up[a]);
+                            a++;
                         }
                       else
                         {
-                            SendKeys.Send("draw up");
+                        
+                            SendKeys.Send(up[a]);
                             a = 0;
                         }
                         SendKeys.Send("{ENTER}");
                         break;
                     case 2:
-                       
-                        if (b != 1)
+
+                        if (b != left.Count-1 )
                         {
-                            SendKeys.Send("dl");
-                            b = 1;
+                            SendKeys.Send(left[b]);
+                            b++;
                         }
-                       else
+                        else
                         {
-                            SendKeys.Send("draw left");
+                         
+                            SendKeys.Send(left[b]); 
                             b = 0;
                         }
                         SendKeys.Send("{ENTER}");
                         break;
                     case 3:
-                        if (c != 1)
+                        if (c != right.Count-1 )
                         {
-                            SendKeys.Send("dr");
-                            c = 1;
+                            SendKeys.Send(right[c]);
+                            c++;
                         }
                         else
                         {
-                            SendKeys.Send("draw right");
+                          
+                            SendKeys.Send(right[c]);
                             c = 0;
                         }
-                      
+
                         SendKeys.Send("{ENTER}");
                         break;
                     case 4:
-                        if (b != 1)
+                        if (d != down.Count -1)
                         {
-                            SendKeys.Send("dd");
-                            b = 1;
+                            SendKeys.Send(down[d]);
+                            d++;
                         }
                         else
                         {
-                            SendKeys.Send("draw down");
-                            b = 0;
+                         
+                            SendKeys.Send(down[d]);
+                            d = 0;
                         }
                         SendKeys.Send("{ENTER}");
                         break;
